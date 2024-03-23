91f0beb1f9de11534679a68170ca6fef8d519094
capybare53 <bayraa093@gmail.com>
2024-03-23 19:50:18 +0300

    login page added

---
+++ start/start.js
@@ -0,0 +1,25 @@
+let login_a = document.querySelector('.login-a').children[0];
+let signup_a = document.querySelector('.signup-a').children[0];
+
+login_a.addEventListener("click", function() {
+   document.querySelector('.login').setAttribute("style", "display: block;");
+   document.querySelector('.signup').setAttribute("style", "display: none;");
+});
+
+signup_a.addEventListener("click", function() {
+   document.querySelector('.login').setAttribute("style", "display: none;");
+   document.querySelector('.signup').setAttribute("style", "display: block;");
+});
+
+let login_btn = document.querySelector('.login-btn');
+let signup_btn = document.querySelector('.signup-btn');
+
+login_btn.addEventListener("click", function() {
+   let name = document.querySelector('#login-username');
+   let password = document.querySelector('#login-password');
+
+   console.log(name.value, password.value);
+
+   
+
+})
